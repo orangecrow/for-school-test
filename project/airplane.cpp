bb4e3a0e94949c49475e579cfc3dd1dd3c420828
Adam <wynalazca47@gmail.com>
2019-05-31 18:13:16 +0200

    a bug

--- project/airplane.cpp
+++ project/airplane.cpp
@@ -1,23 +1,27 @@
 #include"airplane.h"
 int airplane::fly(airfield destination){
-	icv=1;
 	if(distance(get_pos(),destination.get_pos())<=range){
-		my_location->get_out(*this);
+		if(my_location!=NULL)
+			my_location->get_out(*this);
 		my_location=&destination;
 		destination.get_in(*this);
 	}
-	icv=0;
 }
 int airplane::get_in(passenger b){
-	if(icv&&check_luggage(b)){
+	if(check_luggage(b)){
 		people.push_front(b);
 		return 1;
 	}
 	return 0;
 } 
+int airplane::get_out(passenger b){
+	people.remove(b);
+	return 0;
+}
 int airplane::check_luggage(passenger b){
 	list<item>::iterator it;
-	for( it=b.show_items().begin(); it!=b.show_items().end(); ++it ) {
+	auto g = b.show_items();
+	for( it=g.begin(); it!=g.end(); ++it ) {
 		if((*it).get_type()==NOTSAFE)
 			return 0;
 	}
